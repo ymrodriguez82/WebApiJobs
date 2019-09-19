ca4544e3bd8dd0ff8f2fd31511b489313ce2f72c
Yuniesky Mendez Rodriguez <1895543@bdeb.qc.ca>
2019-09-18 23:35:35 -0400

    Ajoute de la class Offre

---
+++ WebApiJbos/Modele/Offre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiJobs.Modele
+{
+    public class Offre
+    {
+        public long Id { get; set; }
+        public string Titre { get; set; }
+        public string Compagnie { get; set; }
+        public string Adresse { get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
